8084ffe5b136073747dcb85bd8501e3ba89d1091
Ingrid Banguero <ingrid_banguero@hotmail.com>
2023-04-22 16:13:39 -0500

    Crear paginas y rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.scss';
+import Home from './pages/Home/Home';
+import Formulario from './pages/Formulario/Formulario';
+import Mentor from './pages/Mentor/Mentor';
+import Eventos from './pages/Eventos/Eventos';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/formulario' element={<Formulario/>} />
+            <Route path='/mentor/:id' element={<Mentor/>} />
+            <Route path='/eventos' element={<Eventos/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Componente1.js
@@ -0,0 +1,7 @@
+import "./Componente1.scss";
+
+const Componente1 = () => {
+    return(<div className="componente1">Componente1</div>)
+}
+
+export default Componente1;
